54440db268f46568be8abd4bbe5b67a59473db0a
Jakob Kogler <jakob.kogler@gmail.com>
2015-02-06 01:09:46 +0100

    Refactoring, better variable names, ...

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import time
+import sys
 
-def rotation(fixed, points, dir=0):
-    p = points[0]
-    points = points[1:]
-    fixed = fixed[:]
-    fixed.append(p)
 
-    q1 = points[-1]
-    #remember last point
+def rotation(fixed_points, points, dir=0):
+    points = points[:]
+    p = fixed_points[-1]
+    rect_point_1 = points[-1] # remember last point
 
-    #translate, rotate, translate
+    # translate, rotate, translate
     if dir==0:
         points = [(p[1]-y+p[0],x-p[0]+p[1]) for (x,y) in points]
     else:
         points = [(y-p[1]+p[0],p[0]-x+p[1]) for (x,y) in points]
 
-    q2 = points[-1]
-    rect_x = [q1[0], q2[0]]
-    rect_y = [q1[1], q2[1]]
+    rect_point_2 = points[-1]
+    rect_x = [rect_point_1[0], rect_point_2[0]]
+    rect_y = [rect_point_1[1], rect_point_2[1]]
 
-    return not any(min(rect_x)<=p[0]<=max(rect_x) and min(rect_y)<=p[1]<=max(rect_y) and (fixed[-1][0]-p[0])**2 + (fixed[-1][1]-p[1])**2 <= len(points)**2 for p in fixed[:-1]), fixed, points
+    allowed = not any(min(rect_x) <= q[0] <= max(rect_x) and min(rect_y) <= q[1] <= max(rect_y)
+              and (p[0] - q[0])**2 + (p[1] - q[1])**2 <= len(points)**2 for q in fixed_points[:-1])
+    return allowed, points
 
 
 def compute_reachable_snake_orientations(n):
-    ret = recursive_search([(0,0)], [(0,i+1) for i in range(n)], [])
-    #print ret
+    ret = recursive_search([(0, 0)], [(0, i+1) for i in range(n)], [])
     return len(set(ret))
 
 
@@ -35,31 +34,35 @@ def recursive_search(fixed, points, rotations, rotation_done = 0):
         rot = tuple(rotations)
         rot = min(rot, rot[::-1], tuple(2-r for r in rot), tuple(2-r for r in rot)[::-1])
         value = 0
-        for r in rot: value = 3*value + r
-        orientations.append(value)
-        return orientations
+        for r in rot:
+            value = 3*value + r
+        return [value]
 
-    # try rotation left
-    success, fixed_left, points_left = rotation(fixed, points, 0)
-    if success:
-        orientations += recursive_search(fixed_left, points_left, rotations + [0], 1)
 
-    # try rotation right
+    fixed = fixed + [points.pop(0)]
+
+    # rotation left
+    allowed, points_left = rotation(fixed, points, 0)
+    if allowed:
+        orientations += recursive_search(fixed, points_left, rotations + [0], 1)
+
+    # rotation right
     if rotation_done:
-        success, fixed_right, points_right = rotation(fixed, points, 1)
-        if success:
-            orientations += recursive_search(fixed_right, points_right, rotations + [2], rotation_done)
+        allowed, points_right = rotation(fixed, points, 1)
+        if allowed:
+            orientations += recursive_search(fixed, points_right, rotations + [2], rotation_done)
 
-    # straight
-    fixed.append(points.pop(0))
+    # no rotatio
     orientations += recursive_search(fixed, points, rotations + [1], rotation_done)
 
     return orientations
 
 
-for n in range(20):
-    start_time = time.time()
-    result = compute_reachable_snake_orientations(n)
-    end_time = time.time()
-    print "Result for", n, "is", result
-    print "duration =", end_time - start_time
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        print(compute_reachable_snake_orientations(int(sys.argv[1])))
+    else:
+        for n in range(20):
+            start_time = time.time()
+            result = compute_reachable_snake_orientations(n)
+            print("{:2d}: {:8d} ({:6.2f} seconds)".format(n, result, time.time() - start_time))
